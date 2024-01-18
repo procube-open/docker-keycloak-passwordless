498679e4ebe3e1243e1c8f20362bfd3ba25919ff
Mitsuru Nakakawaji <mitsuru@procube.jp>
2024-01-18 18:56:31 +0900

    pr-231226233427 (#4)
    * commit for 0.0.5-rc.0
    
    * commit for 0.0.5-rc.1
    
    * commit for 0.0.5-rc.2
    
    * commit for 0.0.5-rc.3
    
    * commit for 0.0.5-rc.4
    
    * commit for 0.0.5-rc.5
    
    * commit for 0.0.5-rc.6
    
    * commit for 0.0.5-rc.7
    
    * commit for 0.0.5-rc.8
    
    * commit for 0.0.5-rc.9
    
    * commit for 0.0.5-rc.10
    
    * commit for 0.0.5-rc.11
    
    * commit for 0.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,94 @@
 # auth-keycloak-cr
 
+## 0.0.5
+
+### Patch Changes
+
+- 2568f8e: 表示名カスタマイズ
+- 7d4cb1f: Fix import error
+- 5b73735: enable account-console
+- 0bbf829: docker login ahead
+- 81def65: Fix int() cause error
+- ee0778a: for debug
+- ca8252d: support protocolMappers
+- 07e82a3: new authentication flow
+- 5b08814: デバッグ
+- d6ca2a0: Fix no OIDC\_
+- e03da18: increment PAGE_NO
+- 5964626: sort authenticationFlows
+
+## 0.0.5-rc.11
+
+### Patch Changes
+
+- Fix import error
+
+## 0.0.5-rc.10
+
+### Patch Changes
+
+- sort authenticationFlows
+
+## 0.0.5-rc.9
+
+### Patch Changes
+
+- new authentication flow
+
+## 0.0.5-rc.8
+
+### Patch Changes
+
+- enable account-console
+
+## 0.0.5-rc.7
+
+### Patch Changes
+
+- support protocolMappers
+
+## 0.0.5-rc.6
+
+### Patch Changes
+
+- increment PAGE_NO
+
+## 0.0.5-rc.5
+
+### Patch Changes
+
+- デバッグ
+
+## 0.0.5-rc.4
+
+### Patch Changes
+
+- for debug
+
+## 0.0.5-rc.3
+
+### Patch Changes
+
+- docker login ahead
+
+## 0.0.5-rc.2
+
+### Patch Changes
+
+- Fix no OIDC\_
+
+## 0.0.5-rc.1
+
+### Patch Changes
+
+- Fix int() cause error
+
+## 0.0.5-rc.0
+
+### Patch Changes
+
+- 表示名カスタマイズ
+
 ## 0.0.4
 
 ### Patch Changes
--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -2,40 +2,67 @@
 import os
 import sys
 import json
+import uuid
 
 with open('/etc/realm-export.json', 'r') as f:
   realm = json.load(f)
 
 for client in realm.get('clients', []):
+  if os.environ.get('KC_WEBGATE_REALM_FRONTEND_URL') and client['webOrigins'] == []:
+    client['webOrigins'] = [os.environ['KC_WEBGATE_REALM_FRONTEND_URL']]
   if client.get('clientId') == 'webgate-web':
     client['secret'] = os.environ['OIDC_CLIENT_SECRET']
+    if os.environ.get('KC_WEBGATE_REALM_RP_URL'):
+      client['rootUrl'] = os.environ['KC_WEBGATE_REALM_RP_URL']
+      client['baseUrl'] = os.environ['KC_WEBGATE_REALM_RP_URL']
+    if os.environ.get('KC_WEBGATE_MAPPERS'):
+      client['protocolMappers'] = list(map(
+        lambda name: {
+          "id": str(uuid.uuid4()),
+          "name": name,
+          "protocol": "openid-connect",
+          "protocolMapper": "oidc-usermodel-attribute-mapper",
+          "consentRequired": "false",
+          "config": {
+            "userinfo.token.claim": "false",
+            "user.attribute": name,
+            "id.token.claim": "true",
+            "access.token.claim": "true",
+            "claim.name": name,
+            "jsonType.label": "String"
+          }
+        },
+        os.environ['KC_WEBGATE_MAPPERS'].split(' ')
+      ))
+
 
 if os.environ.get('OIDC_WEB_AUTHN_POLICY_PASSWORD_RP_ID'):
-  realm['webAuthnPolicyPasswordlessRpId'] = int(os.environ['OIDC_WEB_AUTHN_POLICY_PASSWORD_RP_ID'])
+  realm['webAuthnPolicyPasswordlessRpId'] = os.environ['OIDC_WEB_AUTHN_POLICY_PASSWORD_RP_ID']
+if os.environ.get('OIDC_DISPLAY_NAME'):
+  realm['displayName'] = os.environ['OIDC_DISPLAY_NAME']
+  realm['displayNameHtml'] = os.environ['OIDC_DISPLAY_NAME']
 if os.environ.get('OIDC_ACCESS_TOKEN_LIFESPAN'):
   realm['accessTokenLifespan'] = int(os.environ['OIDC_ACCESS_TOKEN_LIFESPAN'])
 if os.environ.get('OIDC_SSO_SESSION_IDLE_TIMEOUT'):
   realm['ssoSessionIdleTimeout'] = int(os.environ['OIDC_SSO_SESSION_IDLE_TIMEOUT'])
 if os.environ.get('OIDC_SSO_SESSION_MAX_LIFESPAN'):
-  realm['ssoSessionMaxLifespan'] = int(os.environ['SSO_SESSION_MAX_LIFESPAN'])
+  realm['ssoSessionMaxLifespan'] = int(os.environ['OIDC_SSO_SESSION_MAX_LIFESPAN'])
 if os.environ.get('KC_WEBGATE_REALM_FRONTEND_URL'):
-  realm['attributes']['frontendUrl'] = int(os.environ['KC_WEBGATE_REALM_FRONTEND_URL'])
+  realm['attributes']['frontendUrl'] = os.environ['KC_WEBGATE_REALM_FRONTEND_URL']
 if os.environ.get('KC_WEBGATE_REALM_SMTP_SERVER'):
-  realm['smtpServer']['host'] = int(os.environ['KC_WEBGATE_REALM_SMTP_SERVER'])
+  realm['smtpServer']['host'] = os.environ['KC_WEBGATE_REALM_SMTP_SERVER']
   if os.environ.get('KC_WEBGATE_REALM_SMTP_PORT'):
     realm['smtpServer']['port'] = int(os.environ['KC_WEBGATE_REALM_SMTP_PORT'])
   if os.environ.get('KC_WEBGATE_REALM_SMTP_FROM'):
-    realm['smtpServer']['from'] = int(os.environ['KC_WEBGATE_REALM_SMTP_FROM'])
+    realm['smtpServer']['from'] = os.environ['KC_WEBGATE_REALM_SMTP_FROM']
   if os.environ.get('KC_WEBGATE_REALM_SMTP_USER'):
-    realm['smtpServer']['user'] = int(os.environ['KC_WEBGATE_REALM_SMTP_USER'])
+    realm['smtpServer']['user'] = os.environ['KC_WEBGATE_REALM_SMTP_USER']
   if os.environ.get('KC_WEBGATE_REALM_SMTP_PASSWORD'):
-    realm['smtpServer']['password'] = int(os.environ['KC_WEBGATE_REALM_SMTP_PASSWORD'])
+    realm['smtpServer']['password'] = os.environ['KC_WEBGATE_REALM_SMTP_PASSWORD']
   if os.environ.get('KC_WEBGATE_REALM_SMTP_STARTTLS'):
-    realm['smtpServer']['starttls'] = int(os.environ['KC_WEBGATE_REALM_SMTP_STARTTLS'])
+    realm['smtpServer']['starttls'] = os.environ['KC_WEBGATE_REALM_SMTP_STARTTLS']
   if os.environ.get('KC_WEBGATE_REALM_SMTP_SSL'):
-    realm['smtpServer']['ssl'] = int(os.environ['KC_WEBGATE_REALM_SMTP_SSL'])
-
-
+    realm['smtpServer']['ssl'] = os.environ['KC_WEBGATE_REALM_SMTP_SSL']
 with open('/opt/keycloak/data/import/realm.json', 'w') as f:
     json.dump(realm, f, indent=2)
 
